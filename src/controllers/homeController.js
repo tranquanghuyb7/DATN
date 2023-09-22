a94e5c43a49a510cdad92466a0b40bb240c804b2
tranquanghuyb7 <tranquanghuyb7@gmail.com>
2023-09-22 12:01:00 +0700

    #21

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,16 @@
+import db from '../models/index'
 
-let getHomePage = (req, res) => {
-    return res.render('homepage.ejs')
+
+let getHomePage = async (req, res) => {
+    try {
+        let data = await db.User.findAll();
+        return res.render('homepage.ejs', {
+            data: JSON.stringify(data)
+        })
+    } catch (e) {
+        console.log(e)
+    }
+    // return res.render('homepage.ejs')
 }
 
 let getAboutPage = (req, res) => {
