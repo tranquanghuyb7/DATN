0e718f42b9e22eb472bb08245dcd98f588f3cbea
tranquanghuyb7 <tranquanghuyb7@gmail.com>
2023-09-30 15:05:52 +0700

    #25

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,5 @@
 import db from '../models/index'
+import CRUDService from '../services/CRUDService';
 
 
 let getHomePage = async (req, res) => {
@@ -14,10 +15,23 @@ let getHomePage = async (req, res) => {
 }
 
 let getAboutPage = (req, res) => {
-    return res.render('test/test.ejs')
+    return res.render('test/test.ejs');
 }
+
+let getCRUD = (req, res) => {
+    return res.render("crud.ejs");
+}
+
+let postCRUD = async (req, res) => {
+    let message = await CRUDService.createNewUser(req.body);
+    console.log(message)
+    return res.send("post crud from server");
+}
+
 
 module.exports = {
     getHomePage: getHomePage,
     getAboutPage: getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
 }
